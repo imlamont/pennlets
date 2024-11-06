8abec9983007f71997955224917f88465b0ab787
kuomat <kuomatthew20040529@gmail.com>
2024-11-06 17:55:46 -0500

    merging to main

--- client/src/components/MapComponent.jsx
+++ client/src/components/MapComponent.jsx
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from "react";
 import Map, { Marker, NavigationControl, Popup } from "react-map-gl";
-import "mapbox-gl/dist/mapbox-gl.css"; // Required to style the map
+import "mapbox-gl/dist/mapbox-gl.css";
 
 const MapComponent = () => {
+  console.log(import.meta.env.VITE_MAPBOX_TOKEN);
   return (
     <Map
       mapboxAccessToken={import.meta.env.VITE_MAPBOX_TOKEN}
       initialViewState={{
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 14
+        longitude: -100,
+        latitude: 40,
+        zoom: 3
       }}
       style={{width: 600, height: 400}}
       mapStyle="mapbox://styles/mapbox/streets-v9"
